d006df16c4b8fee11ea35ed94469ec8e30cb3fda
Abhishek Mistry <112823095+abhimist@users.noreply.github.com>
2022-10-10 22:44:52 +0530

    Update problem03.js
    Code Updated

--- Conditional_Statements/problem03.js
+++ Conditional_Statements/problem03.js
@@ -3,9 +3,9 @@
 let a = 5;
 let b = 10;
 
-if(a<b){
+if(a > b){
   console.log("a is greater");
-}else if(a>b){
+}else if(a < b){
   console.log("b is greater");
 }else{
   console.log("both equal");
