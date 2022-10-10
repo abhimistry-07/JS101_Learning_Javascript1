44968e15bb9e2b0f37da8bf388eeaaf693690fa2
Abhishek Mistry <112823095+abhimistry-07@users.noreply.github.com>
2022-10-10 23:26:39 +0530

    Update problem01.js

--- Logical_Operators/problem01.js
+++ Logical_Operators/problem01.js
@@ -3,6 +3,6 @@
 let yob = 2000;
 let age = 2022-yob;
 
-age>=13 || age<=19 ? console.log("Teenage") 
-: age>=20 || age<=29 ? console.log("Twenties") 
+age>=13 && age<=19 ? console.log("Teenage") 
+: age>=20 && age<=29 ? console.log("Twenties") 
 : console.log('NA');  
