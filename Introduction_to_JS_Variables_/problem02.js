9eca27a4a98b6828940e014a6657298bbe4a8cd5
Your Name <you@example.com>
2022-10-10 22:17:32 +0530

    Variables & Data Types

---
+++ Introduction_to_JS_Variables_/problem02.js
@@ -0,0 +1,18 @@
+var name = "Abhishek";
+console.log(name);
+
+var fathers name = "Mayurbhai";
+console.log(fathers name);
+
+var mothers name = "Prernaben";
+console.log(mothers name);
+
+
+
+
+//Define a variable called name with your Name as the assigned value
+//Print the value stored in the variable name
+//Change the variable to store your father's name
+//Print the value stored in the variable name
+//Change the variable again to store your mother's name.
+//Print the value stored in the variable name
---
+++ Introduction_to_JS_Variables_/problem03.js
@@ -0,0 +1,20 @@
+let name = "Abhishek";
+let age = 22;
+console.log(name, age);
+
+var nametype = "String";
+let agetype = "Number";
+console.log(nametype, agetype)
+
+
+
+
+
+
+
+
+
+
+//Create a variable name to store your name
+//Create a variable age to store your age
+//Print the values stored in the variable on one line followed by the type of the variable in the next line
