dab92ab07ab85ae0030509deeeb1f8c58f2fc78d
Your Name <you@example.com>
2022-10-10 23:24:51 +0530

    review the code

---
+++ Logical_Operators/problem04.js
@@ -0,0 +1,9 @@
+// Problem 4: Given 3 numbers (all different values), print which is greatest
+
+let a = 2;
+let b = 4;
+let c = 6;
+
+a > b || a > c ? console.log(a, "is greatest")
+: b > a || b > c ? console.log(b, "is greatest")
+: console.log(c, "is greatest");
